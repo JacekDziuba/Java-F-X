734a9c8d217f1af6d871243a74425701ee9a730c
jacek <jaco04@wp.pl>
2019-03-03 19:18:38 +0100

    onAction onKeyReleased

--- Java FX/src/sample/Controller.java
+++ Java FX/src/sample/Controller.java
@@ -1,15 +1,39 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
 
     @FXML
     private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
 
     @FXML
-    public void onButtonClicked() {
-        System.out.println("Hello, " + nameField.getText());
+    public void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(false);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent event) {
+        if (event.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if (event.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
     }
 }
